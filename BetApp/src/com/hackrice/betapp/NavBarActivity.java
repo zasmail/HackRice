ffd4e74c553a9fe3a68fe3f94b5e92e67192d468
Rafa <rmoreno.cesar@gmail.com>
2014-01-25 17:22:38 -0600

    Android app

---
+++ BetApp/src/com/hackrice/betapp/NavBarActivity.java
@@ -0,0 +1,39 @@
+package com.hackrice.betapp;
+
+
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class NavBarActivity  extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actnavbar);
+
+        RadioButton radioButton;
+        radioButton = (RadioButton) findViewById(R.id.btnAll);
+        radioButton.setOnCheckedChangeListener(btnNavBarOnCheckedChangeListener);
+        radioButton = (RadioButton) findViewById(R.id.btnPicture);
+        radioButton.setOnCheckedChangeListener(btnNavBarOnCheckedChangeListener);
+        radioButton = (RadioButton) findViewById(R.id.btnVideo);
+        radioButton.setOnCheckedChangeListener(btnNavBarOnCheckedChangeListener);
+        radioButton = (RadioButton) findViewById(R.id.btnFile);
+        radioButton.setOnCheckedChangeListener(btnNavBarOnCheckedChangeListener);
+        radioButton = (RadioButton) findViewById(R.id.btnMore);
+        radioButton.setOnCheckedChangeListener(btnNavBarOnCheckedChangeListener);
+    }
+
+    private CompoundButton.OnCheckedChangeListener btnNavBarOnCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (isChecked) {
+                Toast.makeText(NavBarActivity.this, buttonView.getText(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
