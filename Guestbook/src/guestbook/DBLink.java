cae58d93c6703e790f986e080a8a70604fb75c81
leftger <grosas@mit.edu>
2014-01-25 22:52:56 -0600

    one more method

--- Guestbook/src/guestbook/DBLink.java
+++ Guestbook/src/guestbook/DBLink.java
@@ -25,4 +25,27 @@ public class DBLink {
 		wagerID = rs.getInt(1);
 		return wagerID;
 	}
+	
+	/*
+	 * adds a wager to the database given the arguments userOne, userTwo, judge, wagerName, rewardOne, 
+	 * rewardTwo, and whoWon.  Returns true if successful, otherwise false
+	 */
+	public boolean addWager(String userOne, String userTwo, String judge, String wagerName, String rewardOne, String rewardTwo,
+			String whoWon) throws SQLException{
+		String statement = "INSERT INTO entries (userOne, userTwo, wagerName, rewardOne, rewardTwo, whoWon) "
+				+ "VALUES( ? , ? , ? , ? , ?, ?)";
+		PreparedStatement stmt = myConn.prepareStatement(statement);
+		stmt.setString(1, userOne);
+		stmt.setString(2, userTwo);
+		stmt.setString(3, wagerName);
+		stmt.setString(4, rewardOne);
+		stmt.setString(5, rewardTwo);
+		stmt.setString(6, whoWon);
+		int success = 2;
+		success = stmt.executeUpdate();
+		if (success == 1) {
+			return true;
+		}
+		return false;
+	}
 }
