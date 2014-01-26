b4cb771f62636a2a5c491391ea47c5dd562aa46d
leftger <grosas@mit.edu>
2014-01-25 23:04:06 -0600

    fixed some stuff

--- Guestbook/src/guestbook/DBLink.java
+++ Guestbook/src/guestbook/DBLink.java
@@ -18,12 +18,18 @@ public class DBLink {
 		return outList;
 	}
 	
+	/*
+	 * gets the wagerID of the wager the user has won, if any, otherwise returns -1
+	 */
 	public int getWhichWagerWon(String user) throws SQLException{
 		int wagerID;
 		ResultSet rs = myConn.createStatement().executeQuery("SELECT wagerID WHERE whoWon=" + user + " UNION "
 				+ "SELECT wagerID WHERE whoWon="+ user);
-		wagerID = rs.getInt(1);
-		return wagerID;
+		if(rs.next()){
+			wagerID = rs.getInt(1);
+			return wagerID;
+		}
+		return -1;
 	}
 	
 	/*
@@ -32,15 +38,16 @@ public class DBLink {
 	 */
 	public boolean addWager(String userOne, String userTwo, String judge, String wagerName, String rewardOne, String rewardTwo,
 			String whoWon) throws SQLException{
-		String statement = "INSERT INTO entries (userOne, userTwo, wagerName, rewardOne, rewardTwo, whoWon) "
-				+ "VALUES( ? , ? , ? , ? , ?, ?)";
+		String statement = "INSERT INTO entries (userOne, userTwo, judge, wagerName, rewardOne, rewardTwo, whoWon) "
+				+ "VALUES( ? , ? , ? , ? , ?, ?, ?)";
 		PreparedStatement stmt = myConn.prepareStatement(statement);
 		stmt.setString(1, userOne);
 		stmt.setString(2, userTwo);
-		stmt.setString(3, wagerName);
-		stmt.setString(4, rewardOne);
-		stmt.setString(5, rewardTwo);
-		stmt.setString(6, whoWon);
+		stmt.setString(3, judge);
+		stmt.setString(4, wagerName);
+		stmt.setString(5, rewardOne);
+		stmt.setString(6, rewardTwo);
+		stmt.setString(7, whoWon);
 		int success = 2;
 		success = stmt.executeUpdate();
 		if (success == 1) {
@@ -48,4 +55,6 @@ public class DBLink {
 		}
 		return false;
 	}
+	
+	
 }
