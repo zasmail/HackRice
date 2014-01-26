b6552740029226e3ef8a0844357149598a231474
josejlm2 <josejlm2@hotmail.com>
2014-01-26 09:11:26 -0600

    latest version

--- BetApp/gen/com/hackrice/betapp/R.java
+++ BetApp/gen/com/hackrice/betapp/R.java
@@ -49,30 +49,31 @@ public final class R {
         public static final int navbar_videoselector=0x7f020016;
     }
     public static final class id {
-        public static final int SignInButton=0x7f090009;
-        public static final int SignUpButton=0x7f090008;
-        public static final int action_settings=0x7f090017;
-        public static final int bet=0x7f090016;
+        public static final int SignInButton=0x7f09000f;
+        public static final int SignUpButton=0x7f09000e;
+        public static final int action_settings=0x7f090018;
+        public static final int bet=0x7f090017;
         public static final int btnAll=0x7f090001;
         public static final int btnFile=0x7f090004;
         public static final int btnMore=0x7f090005;
         public static final int btnPicture=0x7f090002;
         public static final int btnVideo=0x7f090003;
-        public static final int button1=0x7f09000f;
-        public static final int editText1=0x7f09000c;
-        public static final int editText2=0x7f09000b;
-        public static final int editText3=0x7f090011;
-        public static final int exchange=0x7f090013;
+        public static final int button1=0x7f090012;
+        public static final int editText1=0x7f090007;
+        public static final int editText2=0x7f090009;
+        public static final int editText3=0x7f09000b;
+        public static final int exchange=0x7f090014;
         public static final int floatingmenu=0x7f090006;
-        public static final int home=0x7f090012;
-        public static final int imageView2=0x7f09000a;
-        public static final int linearLayout1=0x7f090007;
-        public static final int people=0x7f090015;
+        public static final int home=0x7f090013;
+        public static final int imageView2=0x7f090010;
+        public static final int linearLayout1=0x7f09000d;
+        public static final int people=0x7f090016;
         public static final int radiogroup=0x7f090000;
-        public static final int settings=0x7f090014;
-        public static final int textView1=0x7f09000e;
-        public static final int textView2=0x7f09000d;
-        public static final int textView3=0x7f090010;
+        public static final int settings=0x7f090015;
+        public static final int textView1=0x7f090008;
+        public static final int textView2=0x7f090011;
+        public static final int textView3=0x7f09000a;
+        public static final int textView4=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
